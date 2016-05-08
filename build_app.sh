3fba0d7ece749242b77ed6f47c751d3398d621dc
nullck <nullck@gmail.com>
2016-05-07 21:41:09 -0300

    moved to github

--- build_app.sh
+++ build_app.sh
@@ -2,7 +2,7 @@
 cd /vagrant;
 if [ -d "getninjas_app_sample" ]; then
 	rm -rf getninjas_app_sample
-	git clone https://nullck@bitbucket.org/nullck/getninjas_app_sample.git
+	git clone https://github.com/nullck/getninjas_app_sample.git 
 else
-	git clone https://nullck@bitbucket.org/nullck/getninjas_app_sample.git
+	git clone https://github.com/nullck/getninjas_app_sample.git
 fi
